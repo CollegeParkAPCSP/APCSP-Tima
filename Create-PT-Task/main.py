bd53fdf5f295a3ddb03854a75f2501d243bf98eb
meIsASell <josemiguel.barrios30@gmail.com>
2025-04-01 09:51:39 -0500

    gay: gayest commit in history

--- Create-PT-Task/main.py
+++ Create-PT-Task/main.py
@@ -1,31 +1,39 @@
-def calculate(dailies: list, majors:list, finalw:float = None, dailyWeight = .2, majorWeight = .6, finalWeight = .2):
-    avgDailies = 0
-    avgMajors = 0
-    
-    for i in dailies:
-        avgDailies += int(i)
-    avgDailies /= len(dailies)
+try:
+    def calculate(dailies: list, majors:list, finalw:float = None, dailyWeight = .2, majorWeight = .6, finalWeight = .2):
+        avgDailies = 0
+        avgMajors = 0
+        if classAvg:
+            return round(classAvg*(1-finalWeight) + (finalw * finalWeight),2) if finalw else round(classAvg,2)
+        else:
+            for i in dailies:
+                avgDailies += int(i)
+            avgDailies /= len(dailies)
 
-    for i in majors:
-        avgMajors += int(i)
-    avgMajors /= len(majors)
+            for i in majors:
+                avgMajors += int(i)
+            avgMajors /= len(majors)
 
-    return round((avgDailies * dailyWeight) + (avgMajors * majorWeight) + (finalw * finalWeight),2) if finalw else round((avgDailies * dailyWeight) + (avgMajors * majorWeight),2)
+            return round((avgDailies * dailyWeight) + (avgMajors * majorWeight) + (finalw * finalWeight),2) if finalw else round((avgDailies * dailyWeight) + (avgMajors * majorWeight),2)
+    classAvg=input("Enter your class average or hit enter to enter dailies and majors individually: ")
+    if(classAvg):
+        pass
+    else:
+        dailies = input("Enter your dailies sepparated by commas: ")
+        dailies = dailies.split(",")
+        dailyWeight = float(input("Enter your daily weight: "))
 
-dailies = input("Enter your dailies sepparated by commas: ")
-dailies = dailies.split(",")
-dailyWeight = float(input("Enter your daily weight: "))
+        majors = input("Enter your majors sepparated by commas: ")
+        majors = majors.split(",")
+        majorWeight = float(input("Enter your major weight: "))
 
-majors = input("Enter your majors sepparated by commas: ")
-majors = majors.split(",")
-majorWeight = float(input("Enter your major weight: "))
+    final = input("Enter your final grade sepparated by commas (if none, just enter): ")
 
-final = input("Enter your final grade sepparated by commas (if none, just enter): ")
+    if final:
+        final = float(final)
+        finalWeight = float(input("Enter your final weight: "))
 
-if final:
-    final = float(final)
-    finalWeight = float(input("Enter your final weight: "))
-
-    print("Your final grade is: " + str(calculate(dailies, majors, final, dailyWeight, majorWeight, finalWeight)))
-else:
-    print("Your final grade is: " + str(calculate(dailies, majors, dailyWeight=dailyWeight, majorWeight=majorWeight)))
+        print("Your final grade is: " + str(calculate(dailies, majors, final, dailyWeight, majorWeight, finalWeight)))
+    else:
+        print("Your final grade is: " + str(calculate(dailies, majors, dailyWeight=dailyWeight, majorWeight=majorWeight)))
+except:
+    print("error buddy")
