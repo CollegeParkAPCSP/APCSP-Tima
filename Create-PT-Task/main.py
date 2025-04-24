440c619055c5be1f4185ea5ae9660a14b1380217
timagez <t.gezalov98@gmail.com>
2025-04-24 10:15:49 -0500

    🥺

--- Create-PT-Task/main.py
+++ Create-PT-Task/main.py
@@ -27,17 +27,17 @@ else:
     majors = kk.split(",")
     majorWeight = float(input("Enter your major weight: "))
 
-kkk = float(input("Enter your final grade(if none, just enter): "))
+final = float(input("Enter your final grade(if none, just enter): "))
 
 if classAvg:
-    if kkk:
+    if final:
         finalWeight = float(input("Enter your final weight: "))
-        print("Your final grade is: " + str(calculate(finalw=kkk, finalWeight=finalWeight, classAvg=classAvg)))
+        print("Your final grade is: " + str(calculate(finalw=final, finalWeight=finalWeight, classAvg=classAvg)))
     else:
         print("Your final grade is: " + str(classAvg))
-elif kkk:
+elif final:
     finalWeight = float(input("Enter your final weight: "))
 
-    print("Your final grade is: " + str(calculate(dailies=dailies, majors=majors, finalw=kkk, dailyWeight=dailyWeight, majorWeight=majorWeight, finalWeight=finalWeight)))
+    print("Your final grade is: " + str(calculate(dailies=dailies, majors=majors, finalw=final, dailyWeight=dailyWeight, majorWeight=majorWeight, finalWeight=finalWeight)))
 else:
     print("Your final grade is: " + str(calculate(dailies=dailies, majors=majors, dailyWeight=dailyWeight, majorWeight=majorWeight)))
